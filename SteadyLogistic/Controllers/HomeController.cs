f363f7d5bdae45a9b2b524c1d562c88d2e10a767
Stefan Deliivanov <deliivanov.stefan@hotmail.com>
2021-08-07 19:40:00 +0300

    Created redirects to Member controller if user is not premium and disabled navbars

--- SteadyLogistic/Controllers/HomeController.cs
+++ SteadyLogistic/Controllers/HomeController.cs
@@ -4,14 +4,13 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using SteadyLogistic.Models;
+    using SteadyLogistic.Infrastructure.Extensions;
 
     using static Areas.AreaGlobalConstants.Roles;
 
     [Authorize]
     public class HomeController : Controller
-    {
-        
-        
+    {  
         public HomeController()
         {
             
@@ -19,14 +18,25 @@
 
         public IActionResult Index()
         {
+            if (User.IsMember())
+            {
+                return Redirect("/Member/Member/Index");
+            }
             return View();
         }
 
-        [Authorize(Roles = "AdministratorRoleName, PremiumRoleName, ManagerRoleName")]
+        [Authorize(Roles = "NotAMemberRoleName")]
         public IActionResult News()
         {
             return View();
         }
+
+        [Authorize(Roles = "NotAMemberRoleName")]
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ SteadyLogistic/Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SteadyLogistic.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+
+    using static Areas.AreaGlobalConstants.Roles;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+
+        public static bool IsMember(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(MemberRoleName);
+        }           
+    }
+}
